92a1be13a41bc7a2508715e039f4d8435e43b90c
yifei yan <yanyiphei@gmail.com>
2024-11-10 16:45:57 -0800

    added changes

--- tau_bench/agents/custom_tool_call_data/book_flight_graph.py
+++ tau_bench/agents/custom_tool_call_data/book_flight_graph.py
@@ -6,11 +6,13 @@ from pydantic import Field, BaseModel
 
 ## book flight graph
 
+PREAMBLE = "You are helping the customer to book a flight. "
+
 class UserState(BaseStateModel):
     user_details: Dict = None
 
 get_user_id_node_schema = NodeSchema(
-    node_prompt="You are helping the users book a flight and you need to get the user ID",
+    node_prompt= PREAMBLE + "Right now, you need to get their user details.",
     input_pydantic_model=None,
     state_pydantic_model=UserState,
     tool_registry_or_tool_defs_map=[TOOLS.Think.get_info(), TOOLS.GetUserDetails.get_info()],
@@ -25,7 +27,7 @@ class FlightOrder(BaseStateModel):
     flight_infos: List[FlightInfo] = Field(default_factory=list)
 
 find_flight_node_schema = NodeSchema(
-    node_prompt="You are helping the users book a flight and you need to get the trip info",
+    node_prompt=PREAMBLE + "Right now, you need to help find flights for them.",
     input_pydantic_model=UserInput,
     state_pydantic_model=FlightOrder,
     tool_registry_or_tool_defs_map=[TOOLS.Think.get_info(), TOOLS.SearchDirectFlight.get_info(), TOOLS.SearchOnestopFlight.get_info(),
@@ -41,7 +43,9 @@ class PassengerState(BaseStateModel):
     passengers: List[PassengerInfo] = Field(default_factory=list)
 
 get_passanger_info_schema = NodeSchema(
-    node_prompt="You are helping the users book a flight and you need to get the trip info",
+    node_prompt=(PREAMBLE +
+                 ( "Right now, you need to get the passenger info of all the passengers."
+                 "Each reservation can have at most five passengers. All passengers must fly the same flights in the same cabin." )),
     input_pydantic_model=OrderInput,
     state_pydantic_model=PassengerState,
     tool_registry_or_tool_defs_map=[TOOLS.Think.get_info()],
@@ -56,7 +60,7 @@ class InsuranceState(BaseStateModel):
     add_insurance: Optional[InsuranceValue] = Field(default=None, description="whether to add insurance for all passengers")
 
 ask_for_insurance_node_schema = NodeSchema(
-    node_prompt="You are helping the users book a flight and you need to get the trip info",
+    node_prompt=PREAMBLE + "Right now, you need to ask if they want to add insurance, which is 30 dollars per passenger and enables full refund if the user needs to cancel the flight given health or weather reasons.",
     input_pydantic_model=OrderInput2,
     state_pydantic_model=InsuranceState,
     tool_registry_or_tool_defs_map=[TOOLS.Think.get_info(), TOOLS.Calculate.get_info()],
@@ -75,7 +79,8 @@ class LuggageState(BaseStateModel):
     nonfree_baggages: Optional[int]
 
 luggage_node_schema = NodeSchema(
-    node_prompt="You are helping the users book a flight and you need to get the trip info",
+    node_prompt= PREAMBLE + ("Right now, you need to ask how many luggages to check. "
+                             "If the booking user is a regular member, 0 free checked bag for each basic economy passenger, 1 free checked bag for each economy passenger, and 2 free checked bags for each business passenger. If the booking user is a silver member, 1 free checked bag for each basic economy passenger, 2 free checked bag for each economy passenger, and 3 free checked bags for each business passenger. If the booking user is a gold member, 2 free checked bag for each basic economy passenger, 3 free checked bag for each economy passenger, and 3 free checked bags for each business passenger. Each extra baggage is 50 dollars."),
     input_pydantic_model=OrderInput3,
     state_pydantic_model=LuggageState,
     tool_registry_or_tool_defs_map=[TOOLS.Think.get_info(), TOOLS.Calculate.get_info()],
@@ -96,7 +101,8 @@ class PaymentState(BaseStateModel):
     payments: List[PaymentMethod] = Field(default_factory=list)
 
 payment_node_schema = NodeSchema(
-    node_prompt="You are helping the users book a flight and you need to get the trip info",
+    node_prompt=PREAMBLE + ("Right now, you need to get the payment information"
+                            "Each reservation can use at most one travel certificate, at most one credit card, and at most three gift cards. The remaining amount of a travel certificate is not refundable. All payment methods must already be in user profile for safety reasons."),
     input_pydantic_model=OrderInput4,
     state_pydantic_model=PaymentState,
     tool_registry_or_tool_defs_map=[TOOLS.Think.get_info(), TOOLS.Calculate.get_info()],
@@ -116,7 +122,7 @@ class BookingState(BaseStateModel):
     is_booking_successfull: bool = False
 
 book_flight_node_schema = NodeSchema(
-    node_prompt="You are helping the users book a flight and you need to get the trip info",
+    node_prompt=PREAMBLE + "Right now, you have all the data necessary to place the booking.",
     input_pydantic_model=OrderInput5,
     state_pydantic_model=BookingState,
     tool_registry_or_tool_defs_map=[TOOLS.Think.get_info(), TOOLS.BookReservation.get_info()],
