a005b606b4b25d63cb7985e448b16c0c0f3b7a07
yifei yan <yanyiphei@gmail.com>
2024-11-10 18:58:42 -0800

    added changes

--- tau_bench/agents/custom_tool_call_data/book_flight_graph.py
+++ tau_bench/agents/custom_tool_call_data/book_flight_graph.py
@@ -7,6 +7,7 @@ from tau_bench.agents.custom_tool_call_data.types import (
     InsuranceValue,
 )
 import tau_bench.envs.airline.tools as TOOLS
+from tau_bench.agents.custom_tool_call_data.prompts import AirlineNodeSystemPrompt
 from pydantic import Field, BaseModel
 
 ## book flight graph
@@ -20,6 +21,7 @@ class UserState(BaseStateModel):
 
 get_user_id_node_schema = NodeSchema(
     node_prompt=PREAMBLE + "Right now, you need to get their user details.",
+    node_system_prompt=AirlineNodeSystemPrompt,
     input_pydantic_model=None,
     state_pydantic_model=UserState,
     tool_registry_or_tool_defs_map=[
@@ -42,6 +44,7 @@ class FlightOrder(BaseStateModel):
 
 find_flight_node_schema = NodeSchema(
     node_prompt=PREAMBLE + "Right now, you need to help find flights for them.",
+    node_system_prompt=AirlineNodeSystemPrompt,
     input_pydantic_model=UserInput,
     state_pydantic_model=FlightOrder,
     tool_registry_or_tool_defs_map=[
@@ -72,6 +75,7 @@ get_passanger_info_schema = NodeSchema(
             "Each reservation can have at most five passengers. All passengers must fly the same flights in the same cabin."
         )
     ),
+    node_system_prompt=AirlineNodeSystemPrompt,
     input_pydantic_model=OrderInput,
     state_pydantic_model=PassengerState,
     tool_registry_or_tool_defs_map=[TOOLS.Think.get_info(), TOOLS.Calculate.get_info()],
@@ -94,6 +98,7 @@ class InsuranceState(BaseStateModel):
 ask_for_insurance_node_schema = NodeSchema(
     node_prompt=PREAMBLE
     + "Right now, you need to ask if they want to add insurance, which is 30 dollars per passenger and enables full refund if the user needs to cancel the flight given health or weather reasons.",
+    node_system_prompt=AirlineNodeSystemPrompt,
     input_pydantic_model=OrderInput2,
     state_pydantic_model=InsuranceState,
     tool_registry_or_tool_defs_map=[TOOLS.Think.get_info(), TOOLS.Calculate.get_info()],
@@ -120,6 +125,7 @@ luggage_node_schema = NodeSchema(
         "Right now, you need to ask how many luggages to check. "
         "If the booking user is a regular member, 0 free checked bag for each basic economy passenger, 1 free checked bag for each economy passenger, and 2 free checked bags for each business passenger. If the booking user is a silver member, 1 free checked bag for each basic economy passenger, 2 free checked bag for each economy passenger, and 3 free checked bags for each business passenger. If the booking user is a gold member, 2 free checked bag for each basic economy passenger, 3 free checked bag for each economy passenger, and 3 free checked bags for each business passenger. Each extra baggage is 50 dollars."
     ),
+    node_system_prompt=AirlineNodeSystemPrompt,
     input_pydantic_model=OrderInput3,
     state_pydantic_model=LuggageState,
     tool_registry_or_tool_defs_map=[TOOLS.Think.get_info(), TOOLS.Calculate.get_info()],
@@ -147,6 +153,7 @@ payment_node_schema = NodeSchema(
         "Right now, you need to get the payment information"
         "Each reservation can use at most one travel certificate, at most one credit card, and at most three gift cards. The remaining amount of a travel certificate is not refundable. All payment methods must already be in user profile for safety reasons."
     ),
+    node_system_prompt=AirlineNodeSystemPrompt,
     input_pydantic_model=OrderInput4,
     state_pydantic_model=PaymentState,
     tool_registry_or_tool_defs_map=[TOOLS.Think.get_info(), TOOLS.Calculate.get_info()],
@@ -171,6 +178,7 @@ class BookingState(BaseStateModel):
 book_flight_node_schema = NodeSchema(
     node_prompt=PREAMBLE
     + "Right now, you have all the data necessary to place the booking.",
+    node_system_prompt=AirlineNodeSystemPrompt,
     input_pydantic_model=OrderInput5,
     state_pydantic_model=BookingState,
     tool_registry_or_tool_defs_map=[
