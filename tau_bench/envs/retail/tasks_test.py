cca6acdb24a5fa35e9d6dd5639e3b7d9e554dc8b
Anthony Platanios <e.a.platanios@gmail.com>
2024-12-12 13:14:15 -0800

    [Easy] Fix number of t-shirts in retail tasks.

--- tau_bench/envs/retail/tasks_test.py
+++ tau_bench/envs/retail/tasks_test.py
@@ -77,7 +77,7 @@ TASKS_TEST = [
                 },
             ),
         ],
-        outputs=["10"],
+        outputs=["12"],
     ),
     Task(
         annotator="0",
@@ -109,7 +109,7 @@ TASKS_TEST = [
                 },
             ),
         ],
-        outputs=["10"],
+        outputs=["12"],
     ),
     Task(
         annotator="0",
@@ -150,7 +150,7 @@ TASKS_TEST = [
                 },
             ),
         ],
-        outputs=["10"],
+        outputs=["12"],
     ),
     Task(
         annotator="0",
