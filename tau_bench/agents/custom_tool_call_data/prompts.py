60fad9eaa0374cfef66bdd5e86f4885049ba1216
yifei yan <yanyiphei@gmail.com>
2025-01-02 10:19:44 -0800

    added changes

--- tau_bench/agents/custom_tool_call_data/prompts.py
+++ tau_bench/agents/custom_tool_call_data/prompts.py
@@ -68,10 +68,3 @@ class NoAvailableSeatsPrompt(BasePrompt):
 
         assert len(offending_flights) > 0, "No offending flights found"
         return "I can only add new flights that have available seats in the chosen cabin. However, the following flights do not have available seats in the chosen cabin: " + ", ".join([f"{flight_info.flight_number} ({flight_info.cabin})" for flight_info in offending_flights]) + ". I need to choose different flights."
-
-
-    @classmethod
-    def get_output(
-        cls, model_provider, chat_completion, input
-    ):
-        return chat_completion.get_or_stream_message()
