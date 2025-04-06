da216bff4f0c9806ffc7762f3e566c263a104805
Yifei Yan <yanyiphei@gmail.com>
2025-04-05 22:52:20 -0700

    Revert "custom environment support"

--- tau_bench/run.py
+++ tau_bench/run.py
@@ -18,8 +18,7 @@ from tau_bench.envs.user import UserStrategy
 
 
 def run(config: RunConfig) -> List[EnvRunResult]:
-    if config.env is not None:
-        assert config.env in ["retail", "airline"], "Only retail and airline envs are supported"
+    assert config.env in ["retail", "airline"], "Only retail and airline envs are supported"
     assert config.model_provider in provider_list, "Invalid model provider"
     assert config.user_model_provider in provider_list, "Invalid user model provider"
     if config.agent_strategy is not None:
@@ -36,7 +35,6 @@ def run(config: RunConfig) -> List[EnvRunResult]:
     print(f"Loading user with strategy: {config.user_strategy}")
     env = get_env(
         config.env,
-        custom_env=config.custom_env,
         user_strategy=config.user_strategy,
         user_model=config.user_model,
         user_provider=config.user_model_provider,
@@ -69,7 +67,6 @@ def run(config: RunConfig) -> List[EnvRunResult]:
         def _run(idx: int) -> EnvRunResult:
             isolated_env = get_env(
                 config.env,
-                custom_env=config.custom_env,
                 user_strategy=config.user_strategy,
                 user_model=config.user_model,
                 task_split=config.task_split,
