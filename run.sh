74c73e6d2f8ae14a6db4ac0289e88fed80ea2919
Toby Drane <tobydrane@gmail.com>
2025-04-07 16:48:23 +0100

    Push

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 # --- GPT-4o Mini ---
 
 # Airline all
-# python run.py --agent-strategy tool-calling --env airline \
-# --model gpt-4o-mini --model-provider openai \
-# --user-model gpt-4o-mini --user-model-provider openai \
-# --user-strategy llm --max-concurrency 10
+python run.py --agent-strategy tool-calling --env airline \
+--model gpt-4o-mini --model-provider openai \
+--user-model gpt-4o-mini --user-model-provider openai \
+--user-strategy llm --max-concurrency 10
 
 # Retail all
 # python run.py --agent-strategy tool-calling --env retail \
