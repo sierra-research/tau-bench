36e993f71f8a64caa7c9a33a7931f89a58b0a4a5
HennersBro98 <henry.a.broomfield@gmail.com>
2025-04-14 10:28:53 +0100

    revert run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 # --- GPT-4o Mini ---
 
 # Airline all
-python run.py --agent-strategy tool-calling --env retail \
---model gpt-4o --model-provider openai \
---user-model gpt-4o --user-model-provider openai \
---user-strategy llm --max-concurrency 20
+python run.py --agent-strategy tool-calling --env airline \
+--model gpt-4o-mini --model-provider openai \
+--user-model gpt-4o-mini --user-model-provider openai \
+--user-strategy llm --max-concurrency 10
 
 # Retail all
 # python run.py --agent-strategy tool-calling --env retail \
